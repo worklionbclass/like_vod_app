9aea2062bd581916e3c8ae9b6f6ff0d95524feba
Ubuntu <ubuntu@ip-172-31-46-126.ap-northeast-1.compute.internal>
2019-07-22 17:13:11 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(title: params[:title], content: params[:content])
+    
+    redirect_to "/posts/#{@post.id}/show"
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:title], content: params[:content])
+      
+    redirect_to "/posts/#{@post.id}/show"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_back(fallback_lacation: root_path)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "posts#index"
+  
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id/show" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  get "posts/:id/destroy" => "posts#destroy"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
